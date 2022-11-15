39c2ebfae6172a6b0fb54f397b62954a7e181e76
Benjaxmen <b.suarezchavarria@uandresbello.edu>
2022-11-15 05:08:52 -0300

    Solved running issues

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import uvicorn
 from fastapi import FastAPI
+
 from routes.blobs import blob_routes
 from routes.containers import container_routes
 
@@ -7,4 +9,4 @@ app = FastAPI()
 app.include_router(blob_routes, prefix="/storage/blob")
 app.include_router(container_routes, prefix="/storage/container")   
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run("main:app",reload=True,env_file=".env", port=8000)
